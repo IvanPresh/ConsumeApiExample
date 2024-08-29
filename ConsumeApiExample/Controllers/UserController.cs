de9805478ec381c646a78a50d800720dc34cebc8
Ivan presh <mcpreciousshalom@hotmail.com>
2024-08-29 10:08:32 -0700

    This is a consume reqres Api

---
+++ ConsumeApiExample/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using ConsumeApiExample.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumeApiExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IReqresService _reqresService;
+
+        public UserController(IReqresService reqresService)
+        {
+            _reqresService = reqresService;
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _reqresService.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _reqresService.GetUsers();
+            return Ok(users);
+        }
+    }
+}
+    
+
---
+++ ConsumeApiExample/Services/IReqresService.cs
@@ -0,0 +1,10 @@
+﻿using ConsumeAPIExample.DTO;
+
+namespace ConsumeApiExample.Services
+{
+    public interface IReqresService
+    {
+        Task<UserDTO> GetUserByIdAsync(int id);
+        Task<UsersDTO> GetUsers();
+    }
+}
